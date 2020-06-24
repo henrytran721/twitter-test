934f3f205ed79327fc3ba8559e181240b4b05722
Henry Tran <henryhtran07@gmail.com>
2020-06-24 13:34:51 -0700

    remove timeout time

--- client/src/views/my-api-client.js
+++ client/src/views/my-api-client.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const MyApiClient = axios.create({
   baseURL:'https://henri-twitter-test.herokuapp.com',
-  timeout: 1000,
+  timeout: 300000,
   headers: {'X-Custom-Header': 'foobar'}
 });
